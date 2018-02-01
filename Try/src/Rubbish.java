25f425ee452ca3ebd84165778c3af73158255226
Estella Wu <ymwuab@ust.hk>
2018-02-01 16:47:21 +0800

    no message

---
+++ Try/src/Rubbish.java
@@ -0,0 +1,10 @@
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+
+public class Rubbish extends Character {
+
+    public Rubbish(Pane layer, Image image, double x, double y, double dx, double dy) {
+        super(layer, image, x, y, dx, dy);
+    }
+
+}
